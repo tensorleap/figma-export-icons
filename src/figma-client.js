7f160616c2072adb8cefb1096f281e6365b58900
Himanshu Pant <himanshupant127@gmail.com>
2021-07-04 16:13:31 +0530

    fix: :bug: Switched to reading token from process.env
    Changed method to read token directly from process.env instead of accepting it as a parameter.
    Also added JSDoc type hinting and intellisense for using the Figma Client

--- src/figma-client.js
+++ src/figma-client.js
@@ -1,7 +1,11 @@
 const axios = require('axios')
 const figmaApiBase = 'https://api.figma.com/v1'
 
-const Client = (token) => {
+/**
+ * @see {@link https://www.figma.com/developers/api#access-tokens}
+ * @returns {axios.AxiosStatic} An instance of the Axios http client
+ */
+const Client = () => {
 
   const instance = axios.create({
     baseURL: figmaApiBase
@@ -10,7 +14,7 @@ const Client = (token) => {
   instance.interceptors.request.use((conf) => {
     conf.headers = {
       'Content-Type': 'application/json',
-      'X-Figma-Token': token
+      'X-Figma-Token': process.env.FIGMA_ACCESS_TOKEN
     }
     conf.startTime = new Date().getTime()
     return conf
